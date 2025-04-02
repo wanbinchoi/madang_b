574009f0356510357c3eda9262eb85654879d0e8
dodejqn6 <dodejqn6@naver.com>
2025-04-02 11:50:55 +0900

    commit ConnectionProvider 추가

--- madang_b/src/madang/db/ConnectionProvider.java
+++ madang_b/src/madang/db/ConnectionProvider.java
@@ -1,5 +1,59 @@
 package madang.db;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ConnectionProvider {
+	
+	
+	String drive = "oracle.jdbc.driver.OravleDriver";
+	String url = "jdbc:oracle:thin:@172.30.1.80:1521:XE";
+	String user = "c##madang";
+	String pw = "madang";
+	
+	public static Connection getConnection() {
+		
+		Connection conn = null;
+		try {
+			Class.forName(drive);
+			conn = DriverManager.getConnection(url,user,pw);
+			
+		} catch (Exception e) {
+			System.out.println("예외발생 : " + e.getMessage());
+		}
+		
+		return conn;
+	}
+	
+	public static void close(Connectin conn, Statement stmt, ResultSet rs) {
+		try {
+			if(rs != null) {
+				rs.close();
+			}
+			if(stmt != null) {
+				stmt.close();
+			}
+			if(conn != null) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			System.out.println("예외발생 : " + e.getMessage());
+		}
+	}
+	public static void close(Connectin conn, Statement stmt) {
+		try {
+
+			if(stmt != null) {
+				stmt.close();
+			}
+			if(conn != null) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			System.out.println("예외발생 : " + e.getMessage());
+		}
+	}
+
 
 }
