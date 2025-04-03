49a1d04700f7cd1cf707a4e83919dcf5ebdac10a
wanbinchoi <cwb6278@gmail.com>
2025-04-03 09:33:17 +0900

    feat:도서관리 완료

--- madang_b/src/madang/dao/BookDAO.java
+++ madang_b/src/madang/dao/BookDAO.java
@@ -1,5 +1,10 @@
 package madang.dao;
 
 public class BookDAO {
-
+	public void insert() {
+		System.out.println("도서를 추가하였습니다.");
+	}
+	public void update() {
+		System.out.println("도서를 수정하였습니다.");
+	}
 }
